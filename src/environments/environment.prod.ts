b6da812c969127f8ecb47d8b2516d76445e5a55a
Carlos Aguilar Miguel <cagmi@Carloss-MacBook-Air.local>
2018-10-25 09:45:30 +0200

    Fix some details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://worlding-env.qww7wkqqqe.us-east-1.elasticbeanstalk.com/api',
+  apiIp: 'http://ip-api.com/json/?fields=',
+  paypalClientSandbox: 'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R'
 };
